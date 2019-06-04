c07b113bb3a0e2bcc5ffa5bdedc5c3a3b8175dcb
Narayan <vvs@hpe.com>
2019-06-04 05:27:15 -0400

    runtime identification

---
+++ cpp/Runtimeidentification.cpp
@@ -0,0 +1,18 @@
+
+// Run Time Type Identification  
+#include<iostream> 
+using namespace std; 
+class B { }; 
+class D: public B {}; 
+
+int main() 
+{ 
+    B *b = new D; 
+    D *d = dynamic_cast<D*>(b); 
+    if(d != NULL) 
+        cout<<"works"; 
+    else
+        cout<<"cannot cast B* to D*"; 
+    getchar(); 
+    return 0; 
+}
