7459d63257785cd3964d962c2a6c8c9b15574986
Narayan <vvs@hpe.com>
2019-06-10 02:58:28 -0400

    condiftion exmpa

--- design/condition_notify.cpp
+++ design/condition_notify.cpp
@@ -35,3 +35,36 @@ class Get:
 getter.find(sig, cache);
 getter.wait();
 
+class Examplae
+{
+//example
+void Thread()
+   {
+           thread::set_name(boost::str(boost::format("hdl-%1%") % mName));
+
+               while (true)
+               {
+                   Completion* ioc = NULL;
+                   {
+                    boost::mutex::scoped_lock lock(mMutex);
+
+                        while (!mExit && mCompletionQueue.empty())
+                            mCond.wait(lock);
+
+                        if (mExit)
+                            break;
+
+                    ioc = &mCompletionQueue.front();
+                    mCompletionQueue.pop_front();
+                }
+                   ioc->notify();
+               }
+   }
+
+boost::mutex                mMutex;
+boost::condition_variable   mCond;
+bool                        mExit;
+CompletionList            mCompletionQueue;
+boost::thread               mCompletionThread;
+
+};
